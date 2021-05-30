0ee26901b0fe0d6cea8bf8fd53eff0f5a8934c2b
MEXMAT208 <36931013+MEXMAT208@users.noreply.github.com>
2021-05-30 12:15:13 +0300

    Update Krizh.cpp
    Code style

--- Krizh.cpp
+++ Krizh.cpp
@@ -19,16 +19,16 @@ int find_man(int* answer, int k, int N)
 {
     int i;
 
-    for(i=0;i<N;i++)
-        if(answer[i]==k) return i;
+    for(i = 0; i < N; i++)
+        if (answer[i] == k) return i;
 }
 
 int hm_people_more(int* score, int N)
 {
-    int i,k=0;
+    int i, k = 0;
 
-    for(i=0;i<N-1;i++)
-        if(score[i]<score[N-1]) k++;
+    for(i=0; i < N - 1 ; i++)
+        if(score[i] < score[N-1]) k++;
 
     return k;
 }
@@ -37,8 +37,8 @@ int who_is_the_winner(int* numbers)
 {
     int i;
 
-    for(i=1;i<=101;i++)
-        if(numbers[i]==1) return i;
+    for(i = 1; i <= 101; i++)
+        if(numbers[i] == 1) return i;
 
     return -1;
 }
@@ -49,30 +49,30 @@ int what_my_answer(int* score, int* answer, int* numbers, int* how_many_people_w
 
    //if(numbers[1]==0) return 1;
 
-   for(i=1;i<=101;i++)
+   for(i=1; i <= 101; i++)
    {
       numbers[i]++;
-      answer[N-1]=i;
+      answer[N - 1] = i;
 
       k=who_is_the_winner(numbers);
 
-      if(k==-1)
+      if(k == -1)
       {
-          how_many_people_who_have_less[i]=hm_people_more(score,N);
+          how_many_people_who_have_less[i] = hm_people_more(score,N);
       }
       else
       {
-          winner=find_man(answer,k,N);
-          score[winner]+=k;
-          how_many_people_who_have_less[i]=hm_people_more(score,N);
+          winner = find_man(answer,k,N);
+          score[winner] += k;
+          how_many_people_who_have_less[i] = hm_people_more(score,N);
 
-          score[winner]-=k;
+          score[winner] -= k;
       }
 
       numbers[i]--;
    }
 
-   answ=find_max(how_many_people_who_have_less);
+   answ = find_max(how_many_people_who_have_less);
 
    return answ;
 }
@@ -81,7 +81,7 @@ void my_sort(int* numbers, int* answer, int N)
 {
     int i;
 
-    for(i=0;i<N-1;i++)
+    for(i=0; i < N - 1; i++)
         numbers[answer[i]]++;
 }
 
@@ -89,12 +89,12 @@ void init(int* score, int* answer, int* numbers, int N)
 {
     int i;
 
-    for(i=0;i<N;i++)
+    for(i=0; i < N; i++)
         scanf("%d",&score[i]);
-    for(i=0;i<N-1;i++)
+    for(i=0; i < N - 1; i++)
         scanf("%d",&answer[i]);
 
-    for(i=1;i<=101;i++)
+    for(i = 1; i <= 101; i++)
         numbers[i]=0;
 }
 
@@ -105,15 +105,15 @@ int main(void)
 
     scanf("%d",&N);
 
-    score=(int*)malloc(N*sizeof(int));
-    answer=(int*)malloc(N*sizeof(int));
-    numbers=(int*)malloc(101*sizeof(int));
-    how_many_people_who_have_less=(int*)malloc(100*sizeof(int));
+    score = (int*)malloc(N * sizeof(int));
+    answer = (int*)malloc(N * sizeof(int));
+    numbers = (int*)malloc(101 * sizeof(int));
+    how_many_people_who_have_less = (int*)malloc(100*sizeof(int));
 
     init(score,answer,numbers,N);
     my_sort(numbers,answer,N);
 
-    answ=what_my_answer(score,answer,numbers,how_many_people_who_have_less,N);
+    answ = what_my_answer(score,answer,numbers,how_many_people_who_have_less,N);
 
     printf("%d",answ);
 
